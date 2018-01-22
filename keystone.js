483c6b1b92e3de2ee80a9374975caca67a6e125f
Jan Bevers <jan.bevers@kunstmaan.be>
2018-01-22 06:57:29 +0100

    Cleanup

--- keystone.js
+++ keystone.js
@@ -24,16 +24,6 @@ keystone.init({
 // Load your project's Models
 keystone.import('models');
 
-// Setup common locals for your templates. The following are required for the
-// bundled templates and layouts. Any runtime locals (that should be set uniquely
-// for each request) should be added to ./routes/middleware.js
-keystone.set('locals', {
-	_: require('lodash'),
-	env: keystone.get('env'),
-	utils: keystone.utils,
-	editable: keystone.content.editable,
-});
-
 // Load your project's Routes
 keystone.set('routes', require('./routes'));
 
