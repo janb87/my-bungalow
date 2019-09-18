d2324928d9b68643934d18a668a4119dcd99abd7
Jan Bevers <jbevers@sdl.com>
2019-09-18 11:50:35 +0200

    Added "have a look" button on homepage

--- client/pages/home.js
+++ client/pages/home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from '../routes';
 import colors from '../styles/colors';
 import { getJson } from '../utils/ajax';
 import screenSizes from '../styles/screenSizes';
 import { spacingXlg, spacingLg } from '../styles/spacing';
+import Button from '../components/presentation/common/button';
 
-const HomePage = ({ message }) => [
+const HomePage = ({ message, buttonText, lang }) => [
 	<div key="home-page" className="message">
 		<p>{message}</p>
+		<Link route={`bungalow-${lang}`} params={{ lang }}>
+			<Button label={buttonText} />
+		</Link>
 	</div>,
 	<style key="styles" jsx="">{`
 		.message {
@@ -16,16 +21,24 @@ const HomePage = ({ message }) => [
 			height: 100%;
 			margin: 0 ${spacingLg()};
 			width: calc(100% - 100px);
+		}
 
-			font-size: 2em;
+		.message > p {
 			color: ${colors.white};
+			font-size: 2em;
+		}
+
+		.message .button {
+			margin-top: 0;
 		}
 
 		@media (min-width: ${screenSizes.SM_MIN}) {
 			.message {
 				height: calc(100% - 100px);
 				margin: 0 ${spacingXlg()};
+			}
 
+			.message > p {
 				font-size: 2.5em;
 			}
 		}
@@ -33,15 +46,17 @@ const HomePage = ({ message }) => [
 ];
 
 HomePage.getInitialProps = async ({ req, query: { lang }, config }) => {
-	const { message, backgroundImage } = await getJson(
+	const { message, backgroundImage, buttonText } = await getJson(
 		req,
 		`/api/${encodeURIComponent(lang || config.settings.defaultLanguage)}/home-page`
 	);
 	return {
 		message,
 		backgroundImage: backgroundImage || '/img/home-banner.jpg',
+		buttonText,
 		stickMenuToBottom: true,
 		config,
+		lang,
 	};
 };
 
